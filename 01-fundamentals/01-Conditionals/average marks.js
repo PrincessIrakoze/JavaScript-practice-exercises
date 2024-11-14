d684a295966d7f1222b19bae64e8c1f98e63ed35
princess Irakoze <princessirakoze79@gmail.com>
2024-11-14 17:57:23 +0200

    computing the average marks

---
+++ 01-fundamentals/01-Conditionals/average marks.js
@@ -0,0 +1,10 @@
+let marksOfStudents=0
+if(marksOfStudents<=60){
+    console.log('F');
+} else if(marksOfStudents<=70 && marksOfStudents > 60){
+    console.log('D');
+}else if(marksOfStudents<=80 && marksOfStudents>70){
+    console.log('C');
+}else if(marksOfStudents<=90 && marksOfStudents>80){
+    console.log(B);
+}else{console.log('A');}
