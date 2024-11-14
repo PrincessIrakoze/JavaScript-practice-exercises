b2dab12a73a1721ce030d9faad1005046c59d7b0
princess Irakoze <princessirakoze79@gmail.com>
2024-11-14 10:05:25 +0200

    finding the sign of the product of three number

--- 01-fundamentals/01-Conditionals/largest.js
+++ 01-fundamentals/01-Conditionals/largest.js
@@ -6,3 +6,18 @@ if(a>b){
 else{
     console.log(b +" "+'is the largest number');
 }
+//this is practice number two//
+var x =10
+var y =-12
+var z =8
+if (x>0 && y>0 && z>0){
+    alert("the sign is +");
+} else if (x<0 && y<0 && z<0){
+    console.log(" the sign is -")
+} else if (x>0 && y<0 && z<0){
+    console.log ("the sign is +")
+} else if (x<0 && y>0 && z<0){
+    console.log("the sign is +")
+} else
+{console.log("the sign is -")}
+
