0a27d554ded74bf2d6f5234f2eab4dd1f2356146
princess Irakoze <princessirakoze79@gmail.com>
2024-11-14 18:28:25 +0200

    iterating integeres from 0 to 100

---
+++ 01-fundamentals/01-Conditionals/multiples.js
@@ -0,0 +1,10 @@
+for( let x=0;x<=100;x++)
+    if(x % 3 === 0){
+        console.log ("Fizz");
+    }else if(x % 5 === 0){
+        console.log ("Buzz");
+    }else if(x % 3 === 0 && x % 5 === 0){
+        console.log("FizzBuzz");
+    }
+  
+    
