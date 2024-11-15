eb90a15a76defe986b1bb8bfda20bc85f9a71e51
princess Irakoze <princessirakoze79@gmail.com>
2024-11-15 19:13:01 +0200

    making converter

---
+++ 01-fundamentals/01-Variables/temperature converter.js
@@ -0,0 +1,6 @@
+let temperature =100;
+let temperatureInF = (temperature*9)/5 + 32;
+console.log(temperature + '°C is ' + temperatureInF +'°F');
+let fahrenheit =50;
+let fahrenheitInC =((fahrenheit - 32)*5)/9;
+console.log(fahrenheit + '°F is ' + fahrenheitInC + '°C');
