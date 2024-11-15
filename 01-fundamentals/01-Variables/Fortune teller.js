1f6a6442554e6b3a07da8c4fe950e72c0063eacd
princess Irakoze <princessirakoze79@gmail.com>
2024-11-15 11:08:45 +0200

    outputting the fortune to the screen

---
+++ 01-fundamentals/01-Variables/Fortune teller.js
@@ -0,0 +1,7 @@
+let Child=4;
+let partner= 'George';
+let location= 'Kanombe';
+let jobtitle= 'Teacher';
+{console.log('i will be a'+' '+jobtitle+' '+ 'in'+' '+ location+','+ 'and married to'+' '+partner+' '+ 'with'+' '+Child+' '+'kids'
+ )
+} 
