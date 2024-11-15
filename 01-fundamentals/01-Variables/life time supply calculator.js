41d0d6807159e84e3d06a894b0726b3bfeb9c094
princess Irakoze <princessirakoze79@gmail.com>
2024-11-15 18:15:21 +0200

    how many of my favorite snacks i would consume over the course of my remaining life

---
+++ 01-fundamentals/01-Variables/life time supply calculator.js
@@ -0,0 +1,7 @@
+let currentage =21;
+let maximumage =100;
+let estimatednumber =3;
+let yearsleft =maximumage-currentage;
+let totaldays =yearsleft*365;
+let lifetimesupply =totaldays*estimatednumber;
+console.log('i will need '+lifetimesupply+' to last until the ripe old age '+maximumage);
